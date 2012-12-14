b3589caa791bd97608d51e69477c37967e1f2f78
unknown <sergnad@mail.ru>
2012-12-14 19:04:01 +0200

    v1

--- src/com/example/google_maps_api_v2_sample/MainActivity.java
+++ src/com/example/google_maps_api_v2_sample/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.google_maps_api_v2_sample;
 
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
 
+public class MainActivity extends android.support.v4.app.FragmentActivity {
+
+	private GoogleMap mMap;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mMap = ((SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+		mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
 	}
 
 	@Override
